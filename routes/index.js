9a03211cee205129388cec59463a2a375bc0108b
abutler911 <abutler911@gmail.com>
2024-11-22 07:49:16 -0700

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const authenticateToken = require("../middleware/auth");
 
 router.get("/", (req, res) => {
+  res.render("splash", { title: "Welcome to Focus100", layout: false });
+});
+
+router.get("/login", (req, res) => {
   res.render("login", { title: "Login - Focus100" });
 });
 
